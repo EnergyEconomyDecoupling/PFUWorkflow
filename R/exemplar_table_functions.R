3a70714a402b6877f4df0c5c4cb5a44dc34ede96
Matthew Heun <matthew.heun@me.com>
2020-06-28 10:50:47 -0400

    Working on exemplar table.

--- R/exemplar_table_functions.R
+++ R/exemplar_table_functions.R
@@ -34,7 +34,7 @@ sample_exemplar_table_path <- function() {
 #'                This column is filled with the current 3-letter ISO abbreviations for each country.
 #'                Default is "Country".
 #' @param prev_names The name of a column of previous names used for the country.
-#'                   Default is "Prev_names".
+#'                   Default is "Prev.names".
 #'
 #' @return an exemplar table
 #'
@@ -46,24 +46,68 @@ load_exemplar_table <- function(exemplar_table_path = sample_exemplar_table_path
                                 exemplar_table_tab_name = "exemplar_table",
                                 year = "Year",
                                 country = "Country",
-                                prev_names = "Prev_names") {
+                                prev_names = "Prev.names") {
   prelim <- readxl::read_excel(path = exemplar_table_path, sheet = exemplar_table_tab_name)
   # Figure out year columns.
-  # Assume that any column name that can be converted to an integer is a year.
-  year_cols <- colnames(prelim) %>%
-    strtoi()
-  year_cols <- year_cols[!is.na(year_cols)]
-  max_year <- max(year_cols)
-  # Convert each to a string.
-  year_cols <- lapply(year_cols, FUN = function(y) {
-    toString(y)
-  }) %>%
-    # Convert from list to vector.
-    unlist()
+  year_columns <- IEATools::year_cols(prelim, return_names = TRUE)
+  max_year <- year_columns %>%
+    as.numeric() %>%
+    max() %>%
+    as.character()
 
   prelim %>% dplyr::mutate(
-      "{country}" := .data[[toString(max_year)]]
+      "{country}" := .data[[max_year]]
     ) %>%
     # Gather columns of alternative names
-    tidyr::pivot_longer(cols = tidyselect::all_of(year_cols), names_to = year, values_to = prev_names)
+    tidyr::pivot_longer(cols = tidyselect::all_of(year_columns), names_to = year, values_to = prev_names)
 }
+
+
+#' Create exemplar lists from an exemplar table
+#'
+#' An exemplar list is a list of countries or regions
+#' that should be used to complete a country's
+#' final-to-useful allocation table
+#' or its efficiency table.
+#' This function uses an exemplar table
+#' (probably read by `load_exemplar_table()`)
+#' to create exemplar lists.
+#'
+#' The value of this function is a tibble with three columns:
+#' Country, Year, and Exemplars.
+#' Country contains 3-letter ISO country codes
+#' or names of world regions.
+#' Year contains years.
+#' Exemplars contains a list of countries or regions
+#' that should serve as exemplars for the country in that year.
+#'
+#' Order in each of the exemplar lists is important,
+#' because countries and regions will be searched
+#' in the order they appear in the sub-list
+#' when searching for missing allocations and efficiencies.
+#'
+#' @return A tibble containing countries and regions to be searched for missing
+#'         final-to-useful allocation data or final-to-useful efficiency data.
+#'
+#' @export
+#'
+#' @examples
+exemplar_lists <- function(exemplar_table,
+                           country = IEATools::iea_cols$country,
+                           exemplars = "Exemplars",
+                           prev_names = "Prev.names",
+                           exemplar_country = "Exemplar.country",
+                           row_code = "ROW.code",
+                           countries_or_group = "Country.name",
+                           world = "World") {
+  # Construct a list of exemplars from nearby rows in exemplar_table.
+  # Put the list in the Exemplars column.
+  exemplar_table %>%
+    dplyr::mutate(
+      "{countries_or_group}" := NULL,
+      "{exemplars}" := Map(f = function(coun, row){list(coun, row)}, .data[[exemplar_country]], .data[[row_code]])
+    )
+
+
+}
+
--- tests/testthat/test-exemplar_table.R
+++ tests/testthat/test-exemplar_table.R
@@ -12,7 +12,10 @@ test_that("sample_exemplar_table_path() works correctly", {
 test_that("load_exemplar_table() works correctly", {
   et <- load_exemplar_table()
   # Check that certain features are present
+  # The exemplar country for everybody is South Africa,
+  # matching the data bundled with the IEATools package.
   expect_equal(et$Exemplar_Country %>% unique(), "ZAF")
+  # Make sure Ghana has the right name and 3-letter code.
   et %>%
     dplyr::filter(Country == "GHA") %>%
     dplyr::select(Countries_or_Group) %>%
@@ -20,6 +23,7 @@ test_that("load_exemplar_table() works correctly", {
     unlist() %>%
     unname() %>%
     expect_equal("Ghana")
+  # Make sure we have all the column names that we expect
   cn <- et %>%
     colnames()
   expect_true("Country" %in% cn)
@@ -28,4 +32,19 @@ test_that("load_exemplar_table() works correctly", {
   expect_true("Exemplar_Country" %in% cn)
   expect_true("Region_Code" %in% cn)
   expect_true("Countries_or_Group" %in% cn)
+})
+
+
+test_that("exemplar_lists() works as expected", {
+  el <- exemplar_lists(load_exemplar_table())
+
+  # Check Ghana. It should have the same list for every year
+  el %>%
+    dplyr::select(!IEATools::iea_cols$year) %>%
+    dplyr::filter(IEATools::iea_cols$country == "GHA") %>%
+    unique() %>%
+    magrittr::extract2("Exemplars") %>%
+    unlist() %>%
+    expect_equal(list("ZAF", "RoAFR", "Restofworld"))
+
 })
