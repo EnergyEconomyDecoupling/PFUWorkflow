3ad402cf21e362da8f65ddb620bf838fd51804de
Matthew Heun <matthew.heun@me.com>
2020-07-08 07:57:55 -0400

    coun --> countries

--- R/table_functions.R
+++ R/table_functions.R
@@ -92,7 +92,7 @@ assemble_fu_allocation_tables <- function(fu_analysis_folder,
                                           name_of_iea_data_object = "Specified") {
   # Try to load FU allocation tables for each of the countries from disk
   allocation_tables <- load_fu_allocation_tables(fu_analysis_folder = fu_analysis_folder,
-                                                 countries = coun,
+                                                 countries = countries,
                                                  file_suffix = file_suffix,
                                                  use_subfolders = use_subfolders)
 
