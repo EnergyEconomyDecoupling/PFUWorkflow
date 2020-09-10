aae5ef71eeca76a7d8b3732a2eeb9b525bd9beac
MatthewHeun <MatthewHeun@users.noreply.github.com>
2020-09-10 13:53:45 -0400

    Eliminating reports and reports tests.

--- tests/testthat/test-reports.R
+++ tests/testthat/test-reports.R
@@ -1,27 +1,27 @@
-###########################################################
-context("Report Functions")
-###########################################################
-
-test_that("report_source_paths() works as expected", {
-
-  # Get the reports from the default reports directory that was installed
-  files <- list.files(system.file("reports", package = "SEAPSUTWorkflow"), recursive = TRUE, full.names = TRUE)
-  # Find which ones end in .Rmd or .Rnw
-  which_files_are_Rnw <- which(endsWith(files, ".Rnw"), arr.ind = TRUE)
-  which_files_are_Rmd <- which(endsWith(files, ".Rmd"), arr.ind = TRUE)
-  report_indices <- append(which_files_are_Rmd, which_files_are_Rnw) %>%
-    unlist()
-  reports <- files[report_indices]
-
-  paths <- report_source_paths()
-
-  expect_equal(length(reports), 0)
-  expect_equal(length(setdiff(paths, reports)), 0)
-
-
-})
-
-
+# ###########################################################
+# context("Report Functions")
+# ###########################################################
+#
+# test_that("report_source_paths() works as expected", {
+#
+#   # Get the reports from the default reports directory that was installed
+#   files <- list.files(system.file("reports", package = "SEAPSUTWorkflow"), recursive = TRUE, full.names = TRUE)
+#   # Find which ones end in .Rmd or .Rnw
+#   which_files_are_Rnw <- which(endsWith(files, ".Rnw"), arr.ind = TRUE)
+#   which_files_are_Rmd <- which(endsWith(files, ".Rmd"), arr.ind = TRUE)
+#   report_indices <- append(which_files_are_Rmd, which_files_are_Rnw) %>%
+#     unlist()
+#   reports <- files[report_indices]
+#
+#   paths <- report_source_paths()
+#
+#   expect_equal(length(reports), 0)
+#   expect_equal(length(setdiff(paths, reports)), 0)
+#
+#
+# })
+#
+#
 # test_that("report_dest_paths() works as expected", {
 #   rsps <- report_source_paths()
 #   out_paths <- report_dest_paths(report_source_files = rsps, report_dest_folder = "dest_folder")
@@ -36,25 +36,15 @@ test_that("report_source_paths() works as expected", {
 #     tools::file_path_sans_ext()
 #   expect_equal(out_paths_base, rsps_base)
 # })
-
-
-test_that("generate_reports() works as intended", {
-
-  # This is a bogus test for the time being.
-  # Replace with something better when the code is written.
-  expect_true(generate_reports())
-})
-
-
-
-
-# tryCatch({
 #
-#   report_folder <- tempdir(check = TRUE)
-#   # Now try to generate reports.
-#   expect_true(generate_reports(report_source_files = paths, report_dest_folder = report_folder))
 #
-# },
-# finally = {
-#   unlink(report_folder)
+# test_that("generate_reports() works as intended", {
+#
+#   # This is a bogus test for the time being.
+#   # Replace with something better when the code is written.
+#   expect_true(generate_reports())
 # })
+#
+#
+#
+#
