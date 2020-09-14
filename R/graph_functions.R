2b4fa9569ab78bc6ca0f99f23b55799ab8209cf5
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-09-14 14:02:59 +0100

    First attempt at Efficiency Graph code

--- R/graph_functions.R
+++ R/graph_functions.R
@@ -127,3 +127,137 @@ alloc_plots_df <- function(.df,
                               year = year, .values = .values, machine = machine, eu_product = eu_product)
     )
 }
+
+####################################################################################################################################
+
+#' Generate an eta_fu graph
+#'
+#' Creates an eta_fu graph from a completed eta_fu table,
+#' created by the `assemble_eta_fu_tables()` function.
+#'
+#' This function is called repeatedly from `eta_fu_plots_df()`.
+#'
+#' `country`, `machine`, and `destination` form the title of the graph.
+#'
+#' @param .df A data frame comprised of completed final to useful efficiency values - eta.fu
+#' @param country The country for which this graph applies.
+#' @param machine The machine for which this graph applies.
+#' @param eu_product The useful work product for which this graph applies.
+#' @param year See `IEATools::iea_cols`.
+#' @param .values,machine,eu_product See `IEATools::template_cols`.
+#' @param machine_eu_product The name of a combined `machine` and `eu_product` column.
+#'
+#' @return A `ggplot2` graph object
+#'
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' # Make a simple data frame with the expected structure.
+#' tibble::tribble(~Year, ~.values, ~Machine, ~Eu.product,
+#'                 1967, 0.5, "Cars", "MD",
+#'                 1967, 0.5, "Industry static engines", "MD",
+#'                 2020, 0.8, "Cars", "MD",
+#'                 2020, 0.2, "Industry static engines", "MD") %>%
+#'   alloc_graph(country = "Example", ef_product = "Petrol", destination = "Transport")
+eta_fu_graph <- function(.df,
+                         country,
+                         # machine,
+                         # eu_product,
+                         year = IEATools::iea_cols$year,
+                         .values = IEATools::template_cols$.values,
+                         machine = IEATools::template_cols$machine,
+                         eu_product = IEATools::template_cols$eu_product,
+                         machine_eu_product = paste0(machine, "_", eu_product)) {
+  .df %>%
+    dplyr::mutate(
+      "{machine_eu_product}" := paste(.data[[machine]], "->", .data[[eu_product]])
+    ) %>%
+    ggplot2::ggplot(mapping = ggplot2::aes(x = .data[[year]],
+                                           y = .data[[.values]],
+                                           colour = Country))
+  ggplot2::scale_x_continuous(limits = c(1960, 2020), breaks = seq(1960, 2020, by = 10)) +
+    #ggplot2::scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.2)) +
+    ggplot2::ylab("eta.fu [%]") +
+    # ggplot2::ggtitle(paste0(c(country, ef_product, destination),collapse = "\n")) +
+    ggplot2::ggtitle(paste0(c(paste(machine, "->", eu_product), collapse = "\n")) +
+                       MKHthemes::xy_theme() +
+                       ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+                                      legend.title = ggplot2::element_blank(),
+                                      plot.title   = ggplot2::element_text(colour = "gray50", size = 10),
+                                      legend.text  = ggplot2::element_text(size = 10))
+    )
+}
+
+
+#' Create eta_fu graphs in a data frame
+#'
+#' This function adds a column of `ggplot2` graphs to a completed eta_fu tables data frame.
+#' The graphs are stored in a list column named `plots`.
+#'
+#' By default, the completed eta_fu tables data frame is read from a `drake` cache.
+#'
+#' The data frame is grouped by `country`, `machine`, and `eu_product ` and nested prior to making the graphs,
+#' meaning that one allocation graph is constructed for each combination of country, machine, and eu_product.
+#'
+#' @param .df The completed allocation tables data frame. Default is `drake::readd(SEAPSUTWorkflow::target_names$CompletedEfficiencyTables, path = cache_path, character_only = TRUE)`.
+#' @param countries The countries for which allocation plots are to be created.
+#' @param plots The name of the output column containing allocation graphs. Default is "plots".
+#' @param country See `IEATools::iea_cols`.
+#' #@param ef_product,destination See `IEATools::template_cols`.
+#' @param year See `IEATools::iea_cols`. Passed to `alloc_graph()`.
+#' @param .values,machine,eu_product See `IEATools::template_cols`. Passed to `eta_fu_graph()`.
+#'
+#' @return A data frame containing a list column of `ggplot2` allocation graphs.
+#'
+#' @importFrom utils data
+#'
+#' @export
+#'
+#' @examples
+#' # Make a simple data frame with the expected structure.
+#' alloc_table <- tibble::tribble(~Country, ~Year, ~Ef.product, ~Destination,
+#'                                ~.values, ~Machine, ~Eu.product,
+#'                                "GHA", 1971, "Gasoline", "Transport",
+#'                                0.5, "Cars", "MD",
+#'                                "GHA", 1971, "Gasoline", "Transport",
+#'                                0.5, "Trucks", "MD",
+#'                                "GHA", 2020, "Gasoline", "Transport",
+#'                                0.2, "Cars", "MD",
+#'                                "GHA", 2020, "Gasoline", "Transport",
+#'                                0.8, "Trucks", "MD",
+#'                                "ZAF", 1971, "Gasoline", "Transport",
+#'                                0.5, "Cars", "MD",
+#'                                "ZAF", 1971, "Gasoline", "Transport",
+#'                                0.5, "Trucks", "MD",
+#'                                "ZAF", 2020, "Gasoline", "Transport",
+#'                                0.3, "Cars", "MD",
+#'                                "ZAF", 2020, "Gasoline", "Transport",
+#'                                0.7, "Trucks", "MD")
+#' alloc_plots_df(alloc_table, c("GHA", "ZAF"))
+eta_fu_plots_df <- function(.df,
+                            countries,
+                            plots = "plots",
+                            country = IEATools::iea_cols$country,
+                            #ef_product = IEATools::template_cols$ef_product,
+                            #destination = IEATools::template_cols$destination,
+                            year = IEATools::iea_cols$year,
+                            .values = IEATools::template_cols$.values,
+                            machine = IEATools::template_cols$machine,
+                            eu_product = IEATools::template_cols$eu_product) {
+
+  .df %>%
+    dplyr::filter(.data[[country]] %in% countries) %>%
+    dplyr::group_by(.data[[country]],
+                    .data[[eu_product]],
+                    .data[[machine]]) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      "{plots}" := purrr::map(data, eta_fu_graph,
+                              country = .data[[country]],
+                              year = year,
+                              .values = .values,
+                              machine = machine,
+                              eu_product = eu_product)
+    )
+}
