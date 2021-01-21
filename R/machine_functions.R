c93386c42c4ed3b497e79b5c461930b19553d3d7
ZekeMarshall <marshall_zeke@outlook.com>
2021-01-21 14:47:13 +0000

    Added functions get_eta_filepaths() and read_all_eta_files which read machine eta data from dropbox into a tidy tibble.

---
+++ R/machine_functions.R
@@ -0,0 +1,84 @@
+#' Title
+#' "Country"    "Machine"    "Eu.product" "Metric"     "Year"       "Value"
+#' @return
+#' @export
+#'
+#' @examples
+get_eta_filepaths <- function() {
+
+  # Estalishes name of the frontsheet of each machines excel workbook
+  eta_sheet <- "FIN_ETA"
+
+  # Gets project path
+  project_path <- PFUSetup::get_abs_paths()$project_path
+
+  # Gets path to machine data
+  machine_path <- file.path(project_path, "Data", "Machines - Data")
+
+  # Lists path to each machine folder
+  machine_paths <- list.dirs(path = machine_path, recursive = FALSE)
+
+  # Lists each machine excel file in machine_paths
+  machine_filepaths <- list.files(machine_paths, recursive = FALSE, full.names = TRUE, pattern = ".xlsx") %>%
+    as.list()
+
+  # Creates empty list which will contain a list of paths to machine folders
+  # which contain "FIN_ETA" sheets
+  eta_fin_paths <- list()
+
+  # Loops through all paths to check they have a "FIN_ETA" sheet, and appends
+  # to the eta_fin_paths if so
+  for(path in machine_filepaths) {
+    if(eta_sheet %in% readxl::excel_sheets(path)) {
+      eta_fin_paths <- eta_fin_paths %>% append(path)
+    } else {
+      next
+    }
+  }
+}
+
+
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_all_eta_files <- function(eta_fin_paths) {
+
+  # Creates empty tibble to store etas data in
+  etas <- tibble::as_tibble()
+
+  # Loops through each country in eta_fin_paths to add Eta.fu and Phi.u values
+  # to etas tibble
+  for(path in eta_fin_paths) {
+
+    # Reads raw data
+    raw_etas <- readxl::read_excel(path = path, sheet = "FIN_ETA", skip = 1)
+
+    # Figure out year columns.
+    year_columns <- IEATools::year_cols(raw_etas, return_names = TRUE)
+
+    # Pivots year columns into a "Year" column and a "Value" column
+    raw_etas <- raw_etas %>%
+      tidyr::pivot_longer(cols = tidyselect::all_of(year_columns),
+                          names_to = "Year",
+                          values_to = "Value") %>%
+      dplyr::filter(Metric %in% c("Eta.fu", "Phi.u"))
+    # Sets column classes
+    raw_etas$Country <- as.character(raw_etas$Country)
+    raw_etas$Machine <- as.character(raw_etas$Machine)
+    raw_etas$Eu.product <- as.character(raw_etas$Eu.product)
+    raw_etas$Metric <- as.character(raw_etas$Metric)
+    raw_etas$Year <- as.numeric(raw_etas$Year)
+    raw_etas$Value <- as.numeric(raw_etas$Value)
+
+    # Binds values from individual excel FIN_ETA sheet into etas tibble
+    etas <- etas %>% dplyr::bind_rows(raw_etas)
+
+  }
+
+}
+
+
