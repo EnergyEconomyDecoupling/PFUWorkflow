d807c21129180ba07e51245f2783c20465d92abf
ZekeMarshall <marshall_zeke@outlook.com>
2021-02-04 16:51:47 +0000

    Updated metric_functions description

--- R/metric_functions.R
+++ R/metric_functions.R
@@ -1,2 +1,11 @@
-# This script contains functions which caluclate metrics using the PSUT matrices
-# in the PSUT_useful target
+# This script contains functions which uses the PSUT matrices
+# in the PSUT_useful target to calculate the following metrics:
+# Total energy supply (TES) of primary energy
+# Total final consumption (TFC) of final and useful energy
+# Primary-Final, Final-Useful, and Primary-Final efficiencies
+
+# Loads required packages
+library(matsbyname)
+library(matsindf)
+library(tidyverse)
+library(IEATools)
