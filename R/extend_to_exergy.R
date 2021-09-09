667e0a0d9876b97fc6e56b8d3e65e16707c3b6dc
MatthewHeun <matthew.heun@me.com>
2021-09-09 11:39:19 -0400

    extending to exergy

---
+++ R/extend_to_exergy.R
@@ -0,0 +1,12 @@
+#' Gives a file path to a sample phi constants table
+#'
+#' @return A path to a sample phi constants table bundled with this package.
+#'
+#' @export
+#'
+#' @examples
+#' sample_phi_constants_path()
+sample_phi_constants_path <- function() {
+  file.path("extdata", "phi_constants.xlsx") %>%
+    system.file(package = "SEAPSUTWorkflow")
+}
